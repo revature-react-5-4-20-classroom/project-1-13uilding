237035d90245ac75c0c3052b9c29f2a56d184338
deep <hereisdanbrown@gmail.com>
2020-05-28 14:13:18 -0600

    Added App.tsx, index.tsx, and multiple node modules that are going to be needed for the project

--- react-expense-reimbursement-system/src/App.tsx
+++ react-expense-reimbursement-system/src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component<any, any> {
+  render() {
+    return (
+      <Container>
+        <h1>Hello Project 1</h1>
+      </Container>
+    )
+  }
 }
-
-export default App;
